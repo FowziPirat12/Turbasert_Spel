13c2263ce221a1d0d1d4383d758ad8bcf56cdc0a
FowziPirat12 <111962583+FowziPirat12@users.noreply.github.com>
2024-05-23 21:52:45 +0200

    Update Brute.cs

--- Brute.cs
+++ Brute.cs
@@ -2,31 +2,34 @@ using Turbasert_Spel;
 
 namespace Turbasert_Spel2
 {
-    public class Brute: Enemy
+    public class Brute: Enemy //Skapar klassen Brute som ärver från Enemy
     {
-        private int damage = 10;
+        private int damage = 10; //Private medlem för antal skada Brute gör
 
-        public int Damage
+        public int Damage //Egesńskap för att få tillgång till skada
         {
-            get {return damage;}
+            get {return damage;} //returnar skada
         }
-        public Brute (string name, int hp): base (name, hp)
+        
+        public Brute (string name, int hp): base (name, hp) //konstruktur för att skapa en ny Brute  med angivet namn och Hp, som anropar basklassen
         {
             
         }
 
-        public void DealDamages(string target)
+        public void DealDamages(string target)  //Metod för att göra skada på en "target"
         {
-            Console.WriteLine("Brute deals " + damage + " damage to "  + target);
+            Console.WriteLine("Brute deals " + damage + " damage to "  + target); //Skriver ut meddelande att Goblin gör skada på target 
         }
 
-        public override void TakeDamage (int Tdamage)
+        public override void TakeDamage (int Tdamage) ////Överskuggad metod för att hantera när Brute tar skada
+        
         {
-            Hp -= Tdamage; 
+            Hp -= Tdamage; //Hp sjunker när Brute tar skada
         }
 
-        public override void Print()
+        public override void Print() //Överskuddad metod som skriver ut information om Brute
         {
+            //Skriver ut Enemy Namn och Hp
             Console.WriteLine($"Name: {name}");
             Console.WriteLine($"HP: {hp}");
         } 
